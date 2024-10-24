2d3a043177dc55adf99cb53eca5699e435e57d2a
Higor Carniato <higorzen77@gmail.com>
2024-10-24 07:11:44 -0300

    rate mina gem
    rate mina gem

--- Global.cs
+++ Global.cs
@@ -31,8 +31,8 @@ namespace COServer
         LUCKY_TIME_CRIT_RATE_PHYSICAL = .15,//One in 20 monsters hit with lucky time will take double dmg
         LUCKY_TIME_CRIT_RATE_MAGIC = .7,//One in 20 monsters hit with lucky time will take double dmg
         LUCKY_TIME_CRIT_RATE_MONSTER = 5,
-        MINING_DROP_GEMS = 0.4,
-        MINING_DROP_GEMS_SUPER = 0.1,
+        MINING_DROP_GEMS = 0.3,
+        MINING_DROP_GEMS_SUPER = 0.05,
         MINING_DROP_GEMS_REFIND = 0.2,
         MINING_DROP_DRAGONBALL = 0;
 
