87b6dc942028935be6fbd093a3249d7ec8680993
Higor <higorzen77@gmail.com>
2025-02-08 13:00:51 -0300

    2.0.2
    SurpriseBox, refeito.

--- Game/MsgServer/SurpriseBox.cs
+++ Game/MsgServer/SurpriseBox.cs
@@ -1,36 +1,89 @@
 ﻿using COServer.Client;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace COServer.Game.MsgServer
 {
     public class SurpriseBox
     {
-        static List<uint> High = new List<uint>()
+        static List<uint> VeryHigh1Socket = new List<uint>() // itens 1 socket.
         {
-            720611,723863,723860,723864,723865,720028,720027,722057,711083,723094,1200000,723727,
-            181955, 182635, 191305,200005,754999,753999,753003,753001,751999,751001,751003,730006
+            113013, 114023, 117003, 118003, 120003, 121003, 130020, 131013, 133003, 134003, 141003, 142003, 150003, 152013, 160013, 410003, 420003, 421003
+
         };
-        static List<uint> Mid = new List<uint>()
+
+        static List<uint> VeryHigh2Socket = new List<uint>() // itens 2 socket.
         {
-            720049,723342,723342,753999,753099,754999,780001,720027,722057,711083,723094,1200000,1200001,1200002,1100009,1100006,200312,200311,200310,
-            181955, 182635, 191305,200005,754999,753999,753003,753001,751999,751001,751003,730006
+            113013, 114023, 117003, 118003, 120003, 121003, 130020, 131013, 133003, 134003, 141003, 142003, 150003, 152013, 160013, 410003, 420003, 421003
+
         };
-       
+        static List<uint> VeryHighPlus = new List<uint>() // Plusitens 
+        {
+            113013, 114023, 117003, 118003, 120003, 121003, 130020, 131013, 133003, 134003, 141003, 142003, 150003, 152013, 160013, 410003, 420003, 421003
+
+        };
+        static List<uint> High = new List<uint>() // itens variados raros//
+        {
+            700003, 700013, 700023, 700033, 700043, 700053, 700063, 700073, 723744, 723725, 730004, 723701, 721080
+        };
+        static List<uint> Mid = new List<uint>() // loss casa ganha
+        {
+            1080001, 1088000, 1088001, 1088002, 1060101, 752999, 752099, 752009, 752003, 725024, 725020, 723715, 723716,
+            723712, 723711, 723700, 700001, 700011, 700021, 700031, 700041, 700051, 700061, 700071
+        };
+
         public static void GetReward(GameClient client, ServerSockets.Packet stream)
         {
-            if (Role.MyMath.Success(10))
+            uint reward;
+            int chance = Role.Core.Random.Next(1, 101); 
+
+            if (chance <= 1) 
             {
-                var reward = Mid[Role.Core.Random.Next(0, Mid.Count)];
-                client.Inventory.Add(stream, reward, 1);
-                client.SendSysMesage("You've received a nice reward, check your inventory!");
+                Role.Flags.Gem socktwo = Role.Flags.Gem.EmptySocket;
+                Role.Flags.Gem sockone = Role.Flags.Gem.EmptySocket;
+
+                reward = VeryHigh2Socket[Role.Core.Random.Next(0, VeryHigh2Socket.Count)];
+
+                Console.WriteLine("teste2socket");
+                client.Inventory.Add(stream, reward, 1, 0, 0, 0, sockone, socktwo, false);
+                client.SendSysMesage("You hit the jackpot! Check your inventory for an amazing reward!");
+                Program.SendGlobalPackets.Enqueue(new Game.MsgServer.MsgMessage($"Congratulations [{client.Player.Name}] on winning a rare [ItemTwoSocket] in SurpriseBox!", Game.MsgServer.MsgMessage.MsgColor.white, Game.MsgServer.MsgMessage.ChatMode.System).GetArray(stream));
+                Program.DiscordAPILotery.Enqueue($"```🎉 Congratulations [{client.Player.Name}] on winning a rare [ItemTwoSocket] in SurpriseBox🎁!```");
             }
-            else
+            else if (chance <= 3) 
             {
-                var reward = High[Role.Core.Random.Next(0, High.Count)];
-                client.Inventory.Add(stream, reward, 1);
-                client.SendSysMesage("You've received a nice reward, check your inventory!");
+                Role.Flags.Gem sockone = Role.Flags.Gem.EmptySocket;
+                reward = VeryHigh1Socket[Role.Core.Random.Next(0, VeryHigh1Socket.Count)];
+
+                Console.WriteLine("teste1socket");
+                client.Inventory.Add(stream, reward, 1, 0, 0, 0, sockone, 0, false);
+                client.SendSysMesage("Incredible! You've received a rare reward! Check your inventory.");
+                Program.SendGlobalPackets.Enqueue(new Game.MsgServer.MsgMessage($"Congratulations [{client.Player.Name}] on winning a rare [ItemOneSocket] in SurpriseBox!", Game.MsgServer.MsgMessage.MsgColor.white, Game.MsgServer.MsgMessage.ChatMode.System).GetArray(stream));
+                Program.DiscordAPILotery.Enqueue($"```🔥 Congratulations [{client.Player.Name}] on winning a rare [ItemOneSocket] in SurpriseBox🎁!```");
+            }
+            else if (chance <= 5) 
+            {
+                byte randomValue = Convert.ToByte(Role.Core.Random.Next(3, 6));
+                reward = VeryHighPlus[Role.Core.Random.Next(0, VeryHighPlus.Count)];
+
+                client.Inventory.Add(stream, reward, 1, randomValue, 0, 0, 0, 0, false);
+                client.SendSysMesage("You've unlocked a powerful item! Check your inventory!");
+                Program.SendGlobalPackets.Enqueue(new Game.MsgServer.MsgMessage($"Congratulations [{client.Player.Name}] on winning powerful [+{randomValue}PlusItem] in SurpriseBox!", Game.MsgServer.MsgMessage.MsgColor.white, Game.MsgServer.MsgMessage.ChatMode.System).GetArray(stream));
+                Program.DiscordAPILotery.Enqueue($"```💎 Congratulations [{client.Player.Name}] on winning powerful [+{randomValue}PlusItem] in SurpriseBox🎁!```");
+            }
+            else if (chance <= 15) // 15% de chance para item raro
+            {
+                reward = High[Role.Core.Random.Next(0, High.Count)];
+                client.Inventory.Add(stream, reward, 1, 0, 0, 0, 0, 0, false);
+                client.SendSysMesage("You got something special! Check your inventory!");
+            }
+            else 
+            {
+                reward = Mid[Role.Core.Random.Next(0, Mid.Count)];
+                client.Inventory.Add(stream, reward, 1, 0, 0, 0, 0, 0, false);
+                client.SendSysMesage("🤖 You received a common item, but it’s still useful! Check your inventory.");
             }
         }
+
     }
 }
